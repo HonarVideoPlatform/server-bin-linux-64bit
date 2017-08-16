61b07b5dbb043f471df24eb82e3f356bee285929
HonarVideoPlatform <borhan.d@live.com>
2017-08-17 04:03:01 +0430

    init

---
+++ run/run-ffmpeg-0.10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SYSTEM_INI_FILE=/etc/kaltura.d/system.ini
+if [ -r "$SYSTEM_INI_FILE" ];then
+    . $SYSTEM_INI_FILE
+else
+    echo "I could not source $SYSTEM_INI_FILE. Exiting."
+    exit 1
+fi
+
+KALTURA_BIN=$BASE_DIR/bin
+KALTURA_BIN_DIRS=$KALTURA_BIN
+KALTURA_BIN_FFMPEG=$KALTURA_BIN_DIRS/ffmpeg-0.10-dir
+LD_LIBRARY_PATH=$KALTURA_BIN_FFMPEG/lib $KALTURA_BIN_FFMPEG/ffmpeg "$@"
---
+++ widevine-dir/wvpackager_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -
+#===============================================================================
+#          FILE: wvpackager_wrapper.sh
+#         USAGE: ./wvpackager_wrapper.sh
+#   DESCRIPTION:
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR:  (), @kaltura.com
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED: 10/10/2013 07:54:15 AM EDT
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:@BIN_DIR@/widevine-dir/lib/boost:@BIN_DIR@/widevine-dir/lib/widevine:@BIN_DIR@/ffmpeg-0.6-dir/lib:@BIN_DIR@/widevine-dir/lib/ffmpeg-0.5"
+export LD_LIBRARY_PATH
+@BIN_DIR@/widevine-dir/wvpackager $@
